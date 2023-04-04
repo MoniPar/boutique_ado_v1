4d96a177a096ac720ef279a10068015c83e1b6c8
Monique Parnis <monique.parnis@gmail.com>
2023-04-04 15:55:28 +0000

    Add stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 
@@ -13,7 +14,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
