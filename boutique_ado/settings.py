8490e324ed230b12999a8930113f674c770a6a41
Monique Parnis <monique.parnis@gmail.com>
2023-04-11 14:29:48 +0000

    Set debug to development in environment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['monipar-boutique-ado.herokuapp.com', 'localhost']
 
